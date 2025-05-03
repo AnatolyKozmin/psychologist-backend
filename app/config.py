82b422c669a62fe051c52dc23d2216990493243b
Anatoly <anatolii.kozmin@inbox.ru>
2025-05-03 14:36:31 +0300

    Add config with SQLAlchemyJobStore

---
+++ app/config.py
@@ -0,0 +1,38 @@
+import os
+from typing import List
+
+
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str 
+    ADMIN_IDS: List[int]
+    FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
+    LOG_ROTATION: str = '10 MB'
+    DB_URL: str = 'postgresql+asyncpg://psycho:1234@localhost:5432/psycho_db'
+    BASE_SITE: str
+    TG_API_SITE: str
+    FRONT_SITE: str 
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+    )
+
+    def get_webhook_url(self) -> str:
+        """Вернёт URL вебхука."""
+        return f"{self.BASE_SITE}/webhook"
+    
+    def get_tg_api_url(self) -> str:
+        """Возвращет URL Telegram API."""
+        return f"{self.TG_API_SITE}/bot{self.BOT_TOKEN}"
+    
+
+# Инициализация настроек и планировщика 
+settings = Settings()
+database_url = settings.DB_URL 
+scheduler = AsyncIOScheduler(
+    jobstores={'default': SQLAlchemyJobStore(url=settings.STORE_URL)}
+)
