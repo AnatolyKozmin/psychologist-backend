73e80c7624cc18a9cb3f71baeb744ebd5cb956be
Anatoly <anatolii.kozmin@inbox.ru>
2025-05-04 22:31:31 +0300

    Add BookingDAO and UserDAO

--- app/api/dao.py
+++ app/api/dao.py
@@ -3,4 +3,55 @@ from typing import List
 from fastapi import HTTPException
 from loguru import logger
 from sqlalchemy import select, and_, func 
-from sqlalchemy.exc import 
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
+from dao.base import BaseDAO
+from dao.models import User, Doctor, Booking
+
+
+class UserDAO(BaseDAO[User]):
+    model = User
+
+    @classmethod
+    async def get_user_id(cls, session: AsyncSession, telegram_id: int) -> int | None:
+        query = select(cls.model.id).filter_by(telegram_id=telegram_id)
+        result = await session.execute(query)
+        return result.scalar_one_or_none()
+    
+
+class BookingDAO(BaseDAO[Booking]):
+    model = Booking 
+
+    @classmethod 
+    async def count_user_booking(cls, session: AsyncSession, user_id: int) -> int:
+        query = select(func.count()).where(cls.model.user_id == user_id)
+        result = await session.execute(query)
+        return result.scalar_one()
+    
+    @classmethod 
+    async def get_user_bookings_with_doctor_info(cls, session: AsyncSession, user_id: int):
+        query = (
+            select(cls.model)
+            .options(joinedload(cls.model.doctor))
+            .where(cls.model.user_id == user_id)
+            .order_by(cls.model.day_booking, cls.model.time_booking)
+        )
+        result = await session.execute(query)
+        result_draft = result.unique().scalars().all()
+        data_list = []
+        for info in result_draft():
+            data_list.append({
+                'id': info.id,
+                'day_booking': info.time_booking.strftime("%Y-%m-%d"),
+                'time_booking': info.time_booking.strftime("%H:%M"),
+                'special': info.doctor.special,
+                'doctor_full_name': f'{info.doctor.first_name} {info.doctor.last_name} {info.doctor.patronymic}',
+            })
+        return data_list
+    
+    @classmethod
+    async def generate_working_hours(cls, start_hour=8, end_hour=20):
+        f'''Должно генерировать список рабочих часов, но вопрос нужно ли ? '''
+        ...
+        
